1714d750b12a31d21f9ef3ce62556b8e8073c107
Enoch <49845680+enoch-xxiv@users.noreply.github.com>
2024-04-09 16:08:43 +0100

    add playRound function

--- script.js
+++ script.js
@@ -5,8 +5,38 @@ function getComputerChoice () {
     return choices[choice]
 };
 
-// console.log(getComputerChoice());
-
-// function playRound(playerSelection, computerSelection) {
-
-// };
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection.toLowerCase() == "rock") {
+        if (computerSelection == "Scissors") {
+            return `You win! ${playerSelection} beats ${computerSelection}!`
+        } 
+        else if (computerSelection == "Paper") {
+            return `You lose! ${computerSelection} beats ${playerSelection}!`
+        }
+        else {
+            return `It's a draw!`
+        }
+    }
+    if (playerSelection.toLowerCase() == "paper") {
+        if (computerSelection == "Rock") {
+            return `You win! ${playerSelection} beats ${computerSelection}!`
+        } 
+        else if (computerSelection == "Scissors") {
+            return `You lose! ${computerSelection} beats ${playerSelection}!`
+        }
+        else {
+            return `It's a draw!`
+        }
+    }
+    if (playerSelection.toLowerCase() == "scissors") {
+        if (computerSelection == "Paper") {
+            return `You win! ${playerSelection} beats ${computerSelection}!`
+        } 
+        else if (computerSelection == "Rock") {
+            return `You lose! ${computerSelection} beats ${playerSelection}!`
+        }
+        else {
+            return `It's a draw!`
+        }
+    }
+};
