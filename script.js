d05138b739a5fac0404de2689a19ad095cbeefd8
Enoch <49845680+enoch-xxiv@users.noreply.github.com>
2024-05-28 12:08:41 +0100

    refactor playRound function

--- script.js
+++ script.js
@@ -5,56 +5,77 @@ function getComputerChoice () {
     return choices[choice]
 };
 
-function playRound(playerSelection) {
-    let computerSelection = getComputerChoice();
-
-    if (playerSelection == "rock") {
-        if (computerSelection == "Scissors") {
-            return `You win! ${playerSelection} beats ${computerSelection}!`
-        } 
-        else if (computerSelection == "Paper") {
-            return `You lose! ${computerSelection} beats ${playerSelection}!`
-        }
-        else {
-            return `It's a draw!`
-        }
-    }
-    if (playerSelection == "paper") {
-        if (computerSelection == "Rock") {
-            return `You win! ${playerSelection} beats ${computerSelection}!`
-        } 
-        else if (computerSelection == "Scissors") {
-            return `You lose! ${computerSelection} beats ${playerSelection}!`
-        }
-        else {
-            return `It's a draw!`
-        }
-    }
-    if (playerSelection == "scissors") {
-        if (computerSelection == "Paper") {
-            return `You win! ${playerSelection} beats ${computerSelection}!`
-        } 
-        else if (computerSelection == "Rock") {
-            return `You lose! ${computerSelection} beats ${playerSelection}!`
-        }
-        else {
-            return `It's a draw!`
-        }
-    }
-};
-
 const rock = document.querySelector("#rock");
 const paper = document.querySelector("#paper");
 const scissors = document.querySelector("#scissors");
 
+const playerInfo = document.querySelector("#player-info");
+const computerInfo = document.querySelector("#computer-info");
+const log = document.querySelector("#log")
+
+let playerScore = 0;
+let computerScore = 0;
+
+
+function playRound(playerSelection) {
+    let computerSelection = getComputerChoice();
+    const roundOutcome = document.createElement("p");
+
+    if (playerSelection == "Rock") {
+        if (computerSelection == "Scissors") {
+            playerScore++;
+            log.appendChild((roundOutcome.textContent = `You won this round, ${playerSelection} beats ${computerSelection}!`, roundOutcome));
+        } 
+        else if (computerSelection == "Paper") {
+            computerScore++;
+            log.appendChild((roundOutcome.textContent = `You lost this round, ${computerSelection} beats ${playerSelection}!`, roundOutcome));
+        }
+        else {
+            log.appendChild((roundOutcome.textContent = `It's a`, roundOutcome));
+        }
+    }
+    if (playerSelection == "Paper") {
+        if (computerSelection == "Rock") {
+            playerScore++;
+            log.appendChild((roundOutcome.textContent = `You won this round, ${playerSelection} beats ${computerSelection}!`, roundOutcome));
+        } 
+        else if (computerSelection == "Scissors") {
+            computerScore++;
+            log.appendChild((roundOutcome.textContent = `You lost this round, ${computerSelection} beats ${playerSelection}!`, roundOutcome));
+        }
+        else {
+            log.appendChild((roundOutcome.textContent = `It's a`, roundOutcome));
+        }
+    }
+    if (playerSelection == "Scissors") {
+        if (computerSelection == "Paper") {
+            playerScore++;
+            log.appendChild((roundOutcome.textContent = `You won this round, ${playerSelection} beats ${computerSelection}!`, roundOutcome));
+        } 
+        else if (computerSelection == "Rock") {
+            computerScore++;
+            log.appendChild((roundOutcome.textContent = `You lost this round, ${computerSelection} beats ${playerSelection}!`, roundOutcome));
+        }
+        else {
+            log.appendChild((roundOutcome.textContent = `It's a`, roundOutcome));
+        }
+    }
+};
+
 rock.addEventListener("click", () => {
     playRound("Rock");
+    playerInfo.textContent = `Player: ${playerScore}`
+    computerInfo.textContent = `Computer: ${computerScore}`
 });
 
 paper.addEventListener("click", () => {
     playRound("Paper");
+    playerInfo.textContent = `Player: ${playerScore}`
+    computerInfo.textContent = `Computer: ${computerScore}`
 });
 
 scissors.addEventListener("click", () => {
     playRound("Scissors");
+    playerInfo.textContent = `Player: ${playerScore}`
+    computerInfo.textContent = `Computer: ${computerScore}`
 });
